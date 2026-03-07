a7cbc46081b7bec9610a14d6490239a100e0eaa4
habibthadev <adebayohabib7@gmail.com>
2026-03-07 12:59:54 +0100

    chore(server): initialize Hono backend project with TypeScript, tsup, vitest, eslint, and prettier

---
+++ server/eslint.config.js
@@ -0,0 +1,24 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
+    ignores: ["dist/", "node_modules/"],
+  }
+);
